d6e6b2234b3838a70bd3776608410923341f94ee
velosobr <linoc.veloso@gmail.com>
2020-11-04 21:42:22 -0300

    WIP - parcelable classes

--- Parcelable/app/src/main/java/dominando/android/parcelable/MainActivity.kt
+++ Parcelable/app/src/main/java/dominando/android/parcelable/MainActivity.kt
@@ -1,4 +1,4 @@
-package dominando.android.layouts
+package dominando.android.parcelable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
